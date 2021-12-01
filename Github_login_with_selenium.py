109e9a3ed27d5428a4685db9b616a6346d24a403
Gift Mahlatse Mafomo <mahlatsemafomo@hotmail.com>
2021-12-01 17:41:58 +0200

    Revert "Update Github_login_with_selenium.py"
    This reverts commit b2e0b56eed5f17c1de3bcb1e63ad36d1ac7686ab.

--- Github_login_with_selenium.py
+++ Github_login_with_selenium.py
@@ -1,23 +1,17 @@
 from selenium import webdriver
  
 # Specify the location of ChromeDriver when instantiating
-
 driver = webdriver.Chrome(r'/path/to/chromedriver') 
-
 driver.get("https://github.com/login")
 
 username_account = "enter your username/email"
-
 password = "enter your password"
 
-# Xpath of where the user will fill the mail or username
-
+#xpath of were the user fill the mail or username
 driver.find_element_by_xpath('//*[@id="login_field"]').send_keys(username_account)
   
-# Xpath where the user fill the password
-
+#xpath where the user fill the password
 driver.find_element_by_xpath('//*[@id="password"]').send_keys(password)
   
-# Xpath for clicking login/signing 
-
+# xpath for clicking login/signing 
 driver.find_element_by_xpath('//*[@id="login"]/div[4]/form/div/input[12]').click()
